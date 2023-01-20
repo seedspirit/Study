48f608a3f7435aa8f92fbf9f7e7f3a436d1eb11c
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-01-20 09:37:07 +0900

    Update money.py

--- algorithm/greedy/money.py
+++ algorithm/greedy/money.py
@@ -1,1 +1,25 @@
+'''
+문제 원문: https://www.acmicpc.net/problem/14916
+'''
 
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cnt = 0
+
+while True:
+  if n%5 == 0:
+    cnt += (n//5)
+    break
+  else:
+    n-=2
+    cnt += 1
+
+  if n <0:
+    break
+
+ if n < 0:
+   print(-1)
+ else:
+   print(cnt)
