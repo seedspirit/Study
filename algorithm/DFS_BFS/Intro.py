ca3be48351153005e88b9454f0542b5eb38ea8c9
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-02-01 11:04:02 +0900

    Create Intro.py

---
+++ algorithm/DFS_BFS/Intro.py
@@ -0,0 +1,68 @@
+ ### dfs
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+# dfs 메서드 정의
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited = [False] * 9
+
+# 정의된 DFS 함수 호출
+dfs(graph, 1, visited)
+ 
+ 
+ 
+ 
+ ### bfs
+from collections import deque
+
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+# bfs 메서드 정의
+def bfs(graph, start, visited):
+    # 큐 (queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited = [False] * 9
+
+# 정의된 BFS 함수 호출
+bfs(graph, 1, visited)
